6cf12602333468436c99584c9da5e0df19cf180d
Lulus Young <snys98@outlook.com>
2018-06-16 05:10:22 +0800

    删除已经移植的angularspa相关模块

--- Microex.AngularSpa/UEditor/Handlers/UEditorConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Microex.AngularSpa.UEditor.Handlers
-{
-    internal class UEditorConfig
-    {
-        public UEditorJsonConfig JsonConfig { get; set; }
-        public string WebRootPath { get; set; }
-    }
-}
