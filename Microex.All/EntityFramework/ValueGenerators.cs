14847900aad9664d4a300a25c361ec1ae9b92011
Lulus Young <snys98@outlook.com>
2018-07-10 22:04:44 +0800

    update

---
+++ Microex.All/EntityFramework/ValueGenerators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace Microex.All.EntityFramework
+{
+    public class DatetimeValueGenerator: ValueGenerator<DateTime>
+    {
+        public override DateTime Next(EntityEntry entry)
+        {
+            return DateTime.Now;
+        }
+
+        public override bool GeneratesTemporaryValues { get; } = false;
+    }
+}
